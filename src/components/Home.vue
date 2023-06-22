707ccd36845820e9bdd503428ac846d86e2d5806
Owen Cairns <owencairns@me.com>
2023-06-22 16:56:09 -0400

    another home page update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,30 +4,44 @@ import { ref } from 'vue';
 const weddingVideos = [
   {
     id: 1,
-    thumbnail: 'src/assets/homepageVideos/homevidthumb2.png',
+    thumbnail: 'src/assets/homepageVideos/weddings/homevidthumb2.png',
   },
   {
     id: 2,
-    thumbnail: 'src/assets/homepageVideos/homevidthumb3.png',
+    thumbnail: 'src/assets/homepageVideos/weddings/homevidthumb3.png',
   },
   {
     id: 3,
-    thumbnail: 'src/assets/homepageVideos/homevidthumb4.png',
+    thumbnail: 'src/assets/homepageVideos/weddings/homevidthumb4.png',
   }
 ]
 
 const commercialVideos = [
   {
     id: 1,
-    thumbnail: 'src/assets/homepageVideos/commercialThumb1.png',
+    thumbnail: 'src/assets/homepageVideos/commercial/commercialThumb1.png',
   },
   {
     id: 2,
-    thumbnail: 'src/assets/homepageVideos/commercialThumb2.jpg',
+    thumbnail: 'src/assets/homepageVideos/commercial/commercialThumb2.jpg',
   },
   {
     id: 3,
-    thumbnail: 'src/assets/homepageVideos/commercialThumb3.jpg',
+    thumbnail: 'src/assets/homepageVideos/commercial/commercialThumb3.jpg',
+  }
+];
+const personalPhotos = [
+  {
+    id: 1,
+    thumbnail: 'src/assets/homepageVideos/personal/personalthumbnail1.jpg',
+  },
+  {
+    id: 2,
+    thumbnail: 'src/assets/homepageVideos/personal/personalthumbnail2.jpg',
+  },
+  {
+    id: 3,
+    thumbnail: 'src/assets/homepageVideos/personal/personalthumbnail3.jpg',
   }
 ];
 
@@ -63,6 +77,7 @@ const commercialVideos = [
         <div class="video-grid">
           <div v-for="(video, index) in weddingVideos" :key="index" class="video-item">
             <img :src="video.thumbnail" />
+            <div class="play"><img src="../assets/icons/playIcon.png"/></div>
           </div>
         </div>
       </div>
@@ -71,10 +86,10 @@ const commercialVideos = [
     <!-- New "Commercial" section -->
     <section class="work commercial-section">
       <div class="work-content">
-        <div class="video-grid commercial-videos">
-          <div v-for="(video, index) in commercialVideos" :key="index" class="video-item commercial-video-item">
-            <div class="gray-box"></div>
+        <div class="video-grid">
+          <div v-for="(video, index) in commercialVideos" :key="index" class="video-item">
             <img :src="video.thumbnail" />
+            <div class="play"><img src="../assets/icons/playIcon.png"/></div>
           </div>
         </div>
         <div class="work-description commercial-description">
@@ -91,6 +106,58 @@ const commercialVideos = [
               <router-link to="/commercial" class="link-btn">Explore More</router-link>
             </p>
         </div>
+      </div>
+    </section>
+
+    <section class="work personal-section">
+      <div class="work-content">
+        <div class="work-description">
+          <h3 class="section-heading"><b>Personal</b></h3>
+          <p class="section-description">Capture the essence of your wedding day.
+            We specialize in creating breathtaking videos that preserve the memories of your special moments.
+            Our diverse range of packages caters to weddings of all sizes and styles,
+            ensuring a perfect fit for your needs. From meticulous planning to flawless
+            filming and expert editing, we handle every aspect to create a captivating narrative.
+            Trust us to beautifully capture your vows, speeches, and all the cherished moments.
+            With years of experience, we bring confidence and expertise to deliver videos that will
+            be cherished forever, reflecting the unique story of your love.</p>
+          <p class="section-link">
+            <router-link to="/personal" class="link-btn">Explore More</router-link>
+          </p>
+        </div>
+        <div class="video-grid">
+          <div v-for="(video, index) in personalPhotos" :key="index" class="video-item">
+            <img :src="video.thumbnail" />
+            <div class="play notPlayable"><img src="../assets/icons/playIcon.png"/></div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="about">
+      <div class="company-about">
+        <div class="text">
+          <h2 class="heading">Who Are We?</h2>
+          <div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+            deserunt mollit anim id est laborum.</div>
+        </div>
+      </div>
+      <div class="personal-about">
+        <h3 class="personal-heading">Meet The Team</h3>
+        <div class="about-person noah">
+          <img class="pic-person" src="../assets/noah.jpg" alt="Picture of Noah Ike"/>
+          <div class="description-person noah">
+            <h4 class="name">Noah Ike</h4>
+            <p class="description">This is the about section for noah ike</p>
+          </div>
+        </div>
+        <div class="about-person reagan"></div>
+          <div class="description-person reagan">This is the about for reagan berce</div>
+          <img class="pic-person" src="../assets/reagan.jpg" alt="Picture of Reagan Berce"/>
       </div>
     </section>
 
@@ -158,10 +225,10 @@ html, body {
 .work {
   background-color: #fff;
   color: #1d3051;
-  margin: 0 auto;
   padding-left: 5%;
   padding-right: 5%;
-  padding-bottom: 100px;
+  padding-top: 2%;
+  padding-bottom: 2%;
   position: relative;
 }
 
@@ -196,38 +263,30 @@ html, body {
 }
 
 .work-content {
+  position: relative;
   display: flex;
-  align-items: flex-start;
   justify-content: space-between;
+  gap: 6%;
 }
 
-.work-description {
-  width: 50%;
-}
 
 .work-description h3 {
   font-size: 2rem;
   font-weight: 400;
 }
 
-.work-description p {
-  margin-top: 10px;
-}
-
 .video-grid {
   display: flex;
-  justify-content: center;
-  margin: 20px 10px;
-  border: 1px solid red;
+  justify-content: space-between;
+  gap: 2%;
+  padding: 20px 10px;
+  width: 60%;
 }
 
 .video-item {
-  flex: 0 0 30%; /* Adjust the percentage value as needed */
-  margin: 10px;
-  padding-top: 40%; /* Adjust the padding-top value for the desired aspect ratio */
   position: relative;
-  height: 200px;
-  width: 200px;
+  height: 100%;
+  width: 100vh;
 }
 
 .video-item img {
@@ -238,42 +297,93 @@ html, body {
   height: 100%;
   object-fit: cover;
   transition: 0.4s ease all;
-  filter: grayscale(1);
   border-radius: 1%;
 }
 
-.video-item:hover img {
-  filter: none;
+.video-item .play {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.6);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  opacity: 0;
+  transition: 0.4s ease opacity;
+}
+
+.video-item:hover .play {
+  cursor: pointer;
+  opacity: 1;
+}
+
+.video-item:hover .play.notPlayable {
+  cursor: pointer;
+  opacity: 0;
+}
+
+.play img {
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  height: 20%;
+  width: auto;
 }
 
 .work-content {
   flex-direction: row;
 }
 
-.video-grid.commercial-videos {
-  justify-content: flex-end;
-}
-
-.video-item.commercial-video-item {
-  margin-left: 10px;
-  margin-right: 0;
-}
-
 .work.commercial-section {
   background-color: #e5ecf1;
 }
+
 .commercial-description {
   text-align: right;
 }
 
-.gray-box {
-  width: 100%;
+.about {
+  background-color: #fff;
+  padding-top: 2%;
+  padding-bottom: 2%;
+  position: relative;
+}
+
+.company-about {
+  padding-top: 2%;
+  padding-bottom: 2%;
+  padding-left: 4%;
+  padding-right: 4%;
+  background-image: url(../assets/bannerPhoto.png);
+  background-position: center top; /* Center the background image */
+  background-repeat: no-repeat;
+  background-size: cover; /* Scale the background image to fit the container */
+  text-align: center;
+}
+
+.company-about .text {
+  padding-left: 2%;
+  padding-right: 2%;
+  padding-bottom: 2%;
+  background-color: rgba(0, 0, 0, 0.5);
+  border-radius: 10px;
+}
+.company-about h2 {
+  font-weight: bold;
+  font-size: 2.5rem;
+  margin-bottom: 5px;
+}
+
+.company-about .description {
+  font-size: 1.3rem;
+  font-weight: 500;
+}
+
+.personal-about {
   height: 100%;
-  background-color: #f1f1f1;
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: -1;
+  width: 100%;
 }
 
 .footer {

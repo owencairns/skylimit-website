d8d580e1c93c40ebdb6cf5e8ad89521ca7b0f998
owencairns <owencairns@me.com>
2023-09-05 20:22:35 -0400

    updating meta description and icon

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
-import { useRouter } from 'vue-router';
 import Navbar from './components/Navbar.vue';
 
-const slvBlue = '#1d3051'
-const slvGold = '#F6D7B6'
 
 </script>
 

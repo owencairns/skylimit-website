140d2743cacf58ab65184487647b8bcef19b2459
owencairns <owencairns@me.com>
2025-01-15 13:03:04 -0500

    removing functions to match

--- functions/lib/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-exports.config = {
-    maxFileSize: 5 * 1024 * 1024,
-    allowedFileTypes: ["image/jpeg", "image/png", "video/mp4"],
-    storageBucket: process.env.STORAGE_BUCKET || "your-default-bucket.appspot.com",
-};
-//# sourceMappingURL=config.js.map
